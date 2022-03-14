14889a627596bc1b87c615673e0ff585dc7f7f9e
Andre Guerra <a.guerra@sap.com>
2022-03-14 07:48:45 -0300

    solved execise

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use clap::{Command, Arg};
+
+fn main() {
+    let matches = Command::new("echor")
+        .version("0.0.1")
+        .author("agu3rra")
+        .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .allow_invalid_utf8(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print new line")
+                .takes_value(false)
+        )
+        .get_matches();
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    
+    let ending = if omit_newline { "" } else { "\n" };
+    
+    print!("{}{}", text.join(" "), ending);
+}
